e53362e60115ebf990267cb15b388b95d233b0a7
Michal Molenda <michal@codeapps.io>
2024-02-14 13:20:24 +0100

    fix: remove memory provider as default

--- src/StreplyClient.php
+++ src/StreplyClient.php
@@ -6,7 +6,6 @@ use Streply\Exceptions\InvalidDsnException;
 use Streply\Exceptions\InvalidUserException;
 use Streply\Streply;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Streply\Store\Providers\MemoryProvider;
 use Streply\Responses\Entity;
 
 final class StreplyClient
@@ -23,8 +22,6 @@ final class StreplyClient
 
     public function initialize(): void
     {
-		$this->options['storeProvider'] = new MemoryProvider();
-
         Streply::Initialize($this->dsn, $this->options);
     }
 
